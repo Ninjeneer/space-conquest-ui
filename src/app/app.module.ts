4f1d6e31bee6c39761b42d788c79d72eaabc25da
Loan ALOUACHE <alouache.loan@gmail.com>
2021-08-17 14:18:53 +0200

    Added Menu + Planet view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { PlanetComponent } from './planet/planet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    PlanetComponent
   ],
   imports: [
     BrowserModule,
